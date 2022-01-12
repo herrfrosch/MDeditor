6cd829db4956c6c6da267ca4de2411750ec5e708
Krzysztof Zabski <froschkris@gmail.com>
2022-01-12 21:14:47 +0100

    Added simple markdown h1 formating

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,13 @@ let tCheck = $('#check');
 window.on('load', setInterval(function(){
 
     var rawText = editPlace.val();
+    var edited;
 
-    displayPlace.text(rawText);
+    if (rawText.includes("#")){
+    rawText = rawText.substr(1);
+    edited = "<h1>"+rawText+"</h1>";
+    } else { edited = rawText;}
+
+    displayPlace.html(edited);
 
 }, 10));
