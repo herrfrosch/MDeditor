67519c99cd607174db12bfef0f37b0c00f352333
Krzysztof Zabski <froschkris@gmail.com>
2022-02-01 09:08:35 +0100

    Improved h1-h6 handling

--- scripts/main.js
+++ scripts/main.js
@@ -2,44 +2,110 @@ let editPlace = $('#work-place');
 let displayPlace = $('#display-area');
 let tCheck = $('#check');
 
-window.on('load', setInterval(function(){
+let specialChars = [
+    "#",
+    "- ",
+    "+ ",
+    "=",
+    ">",
+    "  ",
+    "*",
+    "_",
+    ".",
+    "\\",
+    "!",
+    "`",
+    "[",
+    "]"
+];
 
-    var rawText = editPlace.val();
-    var edited;
+window.on('load', setInterval(function () {
 
-    edited = mdParser(rawText);
+    let rawText = editPlace.val();
+    let edited;
+
+    edited = startParser(rawText);
 
     displayPlace.html(edited);
 
 }, 10));
 
 
-function mdParser(text){
+function startParser(text) {
 
     let mdCode = text.split('\n');
 
-    for(var i = 0; i < mdCode.length; i++){
+    mdCode = readToken(mdCode);
 
-        if(mdCode[i].startsWith('# ')){
-            mdCode[i] = mdCode[i].substr(1);
-            mdCode[i] = '<h1>'+mdCode[i]+'</h1>';
-        } else if (mdCode[i].startsWith('## ')){
-            mdCode[i] = mdCode[i].substr(2);
-            mdCode[i] = '<h2>'+mdCode[i]+'</h2>';
-        } else if (mdCode[i].startsWith('### ')){
-            mdCode[i] = mdCode[i].substr(3);
-            mdCode[i] = '<h3>'+mdCode[i]+'</h3>';
-        } 
-        else mdCode[i] = mdCode[i];
+    return mdCode;
+}
 
-        mdCode[i]='<p>'+mdCode[i]+'</p>';
+function readToken(textarr) {
+
+    for (var i = 0; i < textarr.length; i++) {
+
+        for (var a = 0; a < 5; a++) {
+
+            if (textarr[i].startsWith(specialChars[a])) {
+
+                switch (a) {
+
+                    case 0:
+                        textarr[i] = addHeading(textarr[i], 0, 6);
+                        break;
+                    case 1:
+                    case 2:
+                        makeList(textarr, i);
+                        break;
+                    /*case 3:
+                        //underline(textarr[i]);
+                        break;
+                    case 4:
+                        //quote(textarr[i]);
+                        break;
+                    //case 4:*/
+
+
+                }
+            }
+        }
     }
 
-    var mdLength = mdCode[0].length-4;
-
-    mdCode[0] = mdCode[0].substr(3, mdLength);
-
-    //mdCode[0] = mdCode[0]+"</br>";
-
-    return  mdCode;
+    return textarr;
 }
+
+function addHeading(headerText, initA, limitA) {
+
+    for (var a = initA + 1; a < limitA + 1; a++) {
+
+        if (headerText.charAt(a) != specialChars[0]) {
+
+            headerText = headerText.substr(a, headerText.length);
+            headerText = '<h' + a + '>' + headerText + '<h' + a + '>';
+            a = limitA;
+        }
+    }
+
+    return headerText;
+}
+
+/*function makeList(list, listIndex) {
+
+    let counter = 0;
+
+    for (var i = 0; i < (list.length - 1); i++) {
+        if (list[i].startsWith(specialChars[1])) {
+            counter++;
+            if (counter == 1) {
+                list[i] = '<ul><li>' + list[i] + '</li>';
+            } else {
+                list[i-1] = list[i-1].slice(0,list[i-1].length-5);
+                list[i] = '<li>' + list[i] + '</li>';
+            }
+        } else if (counter > 1) {
+            list[i - 1] = '<li>' + list[i] + '</li></ul>';
+            counter = 0;
+        }
+    }
+    return list;
+}*/
